c01c15081231958cdf2ff7e66ca3bcb41516f86f
eptem <mropp1@mail.ru>
2020-08-05 16:51:39 +0500

    test commit

---
+++ CocoaTest/CocoaTest.h
@@ -0,0 +1,25 @@
+//
+//  CocoaTest.h
+//  CocoaTest
+//
+//  Created by Artem Zhornitsky on 05.08.2020.
+//  Copyright © 2020 eptem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaTest.
+FOUNDATION_EXPORT double CocoaTestVersionNumber;
+
+//! Project version string for CocoaTest.
+FOUNDATION_EXPORT const unsigned char CocoaTestVersionString[];
+
+class test {
+    
+    
+public:
+    
+};
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTest/PublicHeader.h>
+
+
