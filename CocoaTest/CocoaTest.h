37c2154def7c7dfb5da46ab37d3741da5d494ac6
eptem <mropp1@mail.ru>
2020-08-05 17:02:21 +0500

    add podspec

--- CocoaTest/CocoaTest.h
+++ CocoaTest/CocoaTest.h
@@ -14,12 +14,6 @@ FOUNDATION_EXPORT double CocoaTestVersionNumber;
 //! Project version string for CocoaTest.
 FOUNDATION_EXPORT const unsigned char CocoaTestVersionString[];
 
-class test {
-    
-    
-public:
-    
-};
 // In this header, you should import all the public headers of your framework using statements like #import <CocoaTest/PublicHeader.h>
 
 
---
+++ CocoaTest/CocoaTest.swift
@@ -0,0 +1,15 @@
+//
+//  CocoaTest.swift
+//  CocoaTest
+//
+//  Created by Artem Zhornitsky on 05.08.2020.
+//  Copyright © 2020 eptem. All rights reserved.
+//
+
+import Foundation
+
+final class CocoaTest {
+    func sayHello() {
+        print("Hello world")
+    }
+}
